5e88a75320bb963b957ab5d25cc93618b6814b89
Rory Vaughn <Rvaughn2525@gmail.com>
2017-05-23 14:34:52 -0500

    table texture and cup functionallity

---
+++ Assets/Scripts/Cup.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cup : MonoBehaviour
+{
+
+
+    private bool Hit = false;
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("DRINK");
+    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+
+	}
+}
