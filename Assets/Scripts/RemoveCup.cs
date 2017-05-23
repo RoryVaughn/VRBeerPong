f611a8975baf588047dc2f5a22dc40eccbe75e9d
Austin Morrell <austinlm19@gmail.com>
2017-05-23 11:56:49 -0500

    starting to work on stuff

---
+++ Assets/Scripts/RemoveCup.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoveCup : MonoBehaviour
+{
+    public Vector3 Position;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        transform.localPosition = Position;
+    }
+}
