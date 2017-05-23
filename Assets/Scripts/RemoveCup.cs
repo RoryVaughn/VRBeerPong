e32c976fd47a43ac88b1f47133f6a46c46ffacb4
Austin Morrell <austinlm19@gmail.com>
2017-05-23 14:35:11 -0500

    Try that on for size (RemoveCup)

--- Assets/Scripts/RemoveCup.cs
+++ Assets/Scripts/RemoveCup.cs
@@ -4,10 +4,37 @@ using UnityEngine;
 
 public class RemoveCup : MonoBehaviour
 {
-    public Vector3 Position;
+    public Vector3 Position = new Vector3(0.055f, 0, -0.422f);
+    private bool con;
+
+    void Start()
+    {
+        con = true;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        transform.localPosition = Position;
+        StartCoroutine(Move());
+    }
+
+    private IEnumerator Move()
+    {
+        for (int i = 0; i < 40; i++)
+        {
+            transform.Translate(0, 1 * Time.deltaTime, 0);
+            yield return 0;
+        }
+        int counter = 0;
+        while (transform.localPosition != Position & con)
+        {
+            transform.localPosition = Vector3.Lerp(transform.localPosition, Position, Time.deltaTime);
+            counter++;
+            if (counter > 500)
+            {
+                con = false;
+            }
+            yield return 0;
+        }
+        StopCoroutine(Move());
     }
 }
