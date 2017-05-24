71440a19a10caab555821a9d4d6f81f56bacd8a0
Austin Morrell <austinlm19@gmail.com>
2017-05-24 12:32:09 -0500

    Wanted to start doing event stuff

---
+++ Assets/Scripts/Events.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public static class Events
+{
+    public static BeerPongEvent CupHit = new BeerPongEvent();
+}
+
+public class BeerPongEvent : UnityEvent
+{
+    
+}
